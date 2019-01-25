9c51af6bb7a10ed9b311be769ae0365aaed090aa
Takumi Nishida <tnishida1@mail.csuchico.edu>
2019-01-25 11:55:12 -0800

    Cleaned up repo, wrote unit tests, and made all functions in plugin public

--- spec/inputs/bitbucket_spec.rb
+++ spec/inputs/bitbucket_spec.rb
@@ -3,9 +3,27 @@ require "logstash/devutils/rspec/spec_helper"
 require "logstash/inputs/bitbucket"
 
 describe LogStash::Inputs::Bitbucket do
-
-  it_behaves_like "an interruptible input plugin" do
-    let(:config) { { "interval" => 100 } }
+  #create a new instance of the class Bitbucket named subject with the following configuration
+  subject { LogStash::Inputs::Bitbucket.new(config) }
+  let(:config) do
+  {
+        'schedule' => { "foo" => "bar", "tak" => "jon" },
+        'scheme' => 'foo',
+        'hostname' => 'foo',
+        'port' => '12345',
+        'token' => 'foo'
+  }
   end
 
-end
+  #assures that we initialized all required configuration vars
+  describe "bitbucket authorization" do
+    it "not raise error" do
+      expect {subject.register}.to_not raise_error
+    end
+  end
+
+
+#  let (:queue) { Queue.new }
+#  describe "handling API response" do
+#    it "handles generic input" do
+
